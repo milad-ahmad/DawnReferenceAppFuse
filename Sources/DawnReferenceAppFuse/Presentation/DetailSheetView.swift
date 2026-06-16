d3f55ec6395e86a54e2086957a02f4393968fbac
milad <ahmadmilad698@gmail.com>
2026-06-16 10:48:01 +0200

    feat: added a detailsheetview

--- Sources/DawnReferenceAppFuse/Presentation/DetailSheetView.swift
+++ Sources/DawnReferenceAppFuse/Presentation/DetailSheetView.swift
@@ -1,22 +1,30 @@
+//
+//  DetailSheetView.swift
+//  dawn-reference-app-fuse
+//
+//  Created by Milad Ahmad on 16-06-2026.
+//
+import SwiftUI
+
 public struct DetailSheetView: View {
     @Environment(\.dismiss) var dismiss
     @State var showNestedAlert = false
-    
+
     let title: String
     let isFullScreen: Bool
-    
+
     public init(title: String, isFullScreen: Bool) {
         self.title = title
         self.isFullScreen = isFullScreen
     }
-    
+
     public var body: some View {
         NavigationStack {
             VStack(spacing: 30) {
-                Text("Testing dismissal and nested presentations.")
+                Text("Testing presentations.")
                     .multilineTextAlignment(.center)
                     .padding()
-                
+
                 Button("Alert") {
                     showNestedAlert = true
                 }
@@ -26,11 +34,13 @@ public struct DetailSheetView: View {
                 .foregroundColor(.white)
                 .cornerRadius(8)
                 .alert("Alert", isPresented: $showNestedAlert) {
-                    Button("OK", role: .cancel) { }
+                    Button("Ok", role: .cancel) {}
                 } message: {
-                    Text("This tests if Skip can show an alert on top of a \(isFullScreen ? "full screen cover" : "sheet") in Android.")
+                    Text(
+                        "This tests if Skip can show an alert on top of a \(isFullScreen ? "full screen cover" : "sheet") in Android."
+                    )
                 }
-                
+
                 Button(action: {
                     dismiss()
                 }) {
@@ -55,3 +65,4 @@ public struct DetailSheetView: View {
             }
         }
     }
+}
