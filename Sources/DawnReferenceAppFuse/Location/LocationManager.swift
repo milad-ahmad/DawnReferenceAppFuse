e3271519f574c1c6c7f08d956cfe7324d63cac4a
milad <ahmadmilad698@gmail.com>
2026-06-17 10:30:16 +0200

    fix: processed feedback

--- Sources/DawnReferenceAppFuse/Location/LocationManager.swift
+++ Sources/DawnReferenceAppFuse/Location/LocationManager.swift
@@ -18,25 +18,23 @@ import SkipKit
 
 @MainActor
 @Observable
-public final class LocationManager: @unchecked Sendable {
-    public var isUpdating = false
-    public var permissionDenied = false
-    public var latitude = 0.0
-    public var longitude = 0.0
-    public var error: Error?
+public final class LocationManager: NSObject {
+    public private(set) var isUpdating = false
+    public private(set) var permissionDenied = false
+    public private(set) var latitude = 37.3230
+    public private(set) var longitude = -122.0323
+    public private(set) var error: Error?
     
     #if os(iOS)
     private let coreLocationManager = CLLocationManager()
-    private var locationDelegate: LocationDelegate?
     #else
     private var updateTask: Task<Void, Never>?
     #endif
     
-    public init() {
+    public override init() {
+        super.init()
         #if os(iOS)
-        let delegate = LocationDelegate(parent: self)
-        self.locationDelegate = delegate
-        self.coreLocationManager.delegate = delegate
+        self.coreLocationManager.delegate = self
         #endif
     }
     
@@ -70,8 +68,9 @@ public final class LocationManager: @unchecked Sendable {
             
             do {
                 let provider = LocationProvider()
-                if let location = try? await provider.fetchCurrentLocation() { self?.update(location.latitude, location.longitude) }
-                for try await event in provider.monitor() { self?.update(event.latitude, event.longitude) }
+                for try await event in provider.monitor() {
+                    self?.update(event.latitude, event.longitude)
+                }
             } catch {
                 self?.error = error
                 self?.isUpdating = false
@@ -98,28 +97,22 @@ public final class LocationManager: @unchecked Sendable {
 }
 
 #if os(iOS)
-private final class LocationDelegate: NSObject, CLLocationManagerDelegate, @unchecked Sendable {
-    var parent: LocationManager?
-    
-    init(parent: LocationManager) {
-        self.parent = parent
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        Task { @MainActor in parent?.update(location.coordinate.latitude, location.coordinate.longitude) }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        guard let LocationError = error as? CLError, LocationError.code != .locationUnknown else { return }
-        Task { @MainActor in
-            if LocationError.code == .denied {
-                parent?.permissionDenied = true
-                parent?.stop()
-            } else {
-                parent?.error = error
+extension LocationManager: @MainActor CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.last else { return }
+            Task { @MainActor in self.update(location.coordinate.latitude, location.coordinate.longitude) }
+        }
+        
+        public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            guard let locationError = error as? CLError, locationError.code != .locationUnknown else { return }
+            Task { @MainActor in
+                if locationError.code == .denied {
+                    self.permissionDenied = true
+                    self.stop()
+                } else {
+                    self.error = error
+                }
             }
         }
-    }
 }
 #endif
