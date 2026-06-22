af70e79bdc3fe89b69add0c67822de107093d736
milad <ahmadmilad698@gmail.com>
2026-06-22 11:33:53 +0200

    feat: created a deeplinkview so users can test the deeplinks

--- Sources/DawnReferenceAppFuse/Deeplink/DeepLinkView.swift
+++ Sources/DawnReferenceAppFuse/Deeplink/DeepLinkView.swift
@@ -1,18 +1,23 @@
-//
-//  SwiftUIView.swift
-//  dawn-reference-app-fuse
-//
-//  Created by Milad Ahmad on 22-06-2026.
-//
-
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct DeepLinkTestView: View {
+    @Environment(\.openURL) public var openURL
+    
+    public init() {}
+    
+    public var body: some View {
+        List {
+            Section("Valid Routes") {
+                Button("Open Home") { openURL(URL(string: "dawnapp://home")!) }
+                Button("Open Location Services") { openURL(URL(string: "dawnapp://location")!) }
+                Button("Open Biometrics") { openURL(URL(string: "dawnapp://biometrics")!) }
+                Button("Open Camera") { openURL(URL(string: "dawnapp://camera")!) }
+            }
+            
+            Section("Error Handling") {
+                Button("Test Invalid Route") { openURL(URL(string: "dawnapp://does-not-exist")!) }
+            }
+        }
+        .navigationTitle("Deep Links")
     }
-}
-
-#Preview {
-    SwiftUIView()
 }
